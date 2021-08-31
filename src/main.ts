46922fd43a608659a70f7b7f5e179103be9ab113
Joshua Chagani <joshua@joshuachagani.com>
2021-08-30 20:41:09 -0500

    💄 Actually toggle between light and dark

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,16 @@ const toggleCanvasButton = document.querySelector<HTMLButtonElement>(
 	'[data-toggle="canvas"]',
 )!
 
+toggleCanvasButton.innerText = 'Show canvas'
+
+function toggleMode() {
+	if (darkMode) {
+		toggleThemeButton.innerText = 'Light mode'
+	} else {
+		toggleThemeButton.innerText = 'Dark mode'
+	}
+}
+
 const svgPath = `M407.822,43.73L407.822,138.025L678.8,138.025L678.8,631.322C678.8,782.436 609.112,985.634 280.5,820.514C250.043,865.627 228.362,895.887 228.362,895.887C228.362,895.887 327.122,980.27 472.502,980.27C617.881,980.27 687.783,930.029 734.658,871.898C781.534,813.766 795.638,720.471 795.638,655.505L795.638,43.73L407.822,43.73Z`
 
 const canvas = document.createElement('canvas')
@@ -78,6 +88,7 @@ function draw() {
 	faviconLink.href = data
 }
 
+toggleMode()
 setInterval(draw, 200)
 
 toggleThemeButton.addEventListener('click', function () {
@@ -92,12 +103,17 @@ toggleThemeButton.addEventListener('click', function () {
 		colorFill = '255, 255, 255'
 		backgroundColor = '0, 0, 0'
 	}
+	toggleMode()
 })
 
 toggleCanvasButton.addEventListener('click', function () {
 	if (canvasWrapper.hasChildNodes()) {
 		canvasWrapper.removeChild(canvas)
+		toggleCanvasButton.innerText = 'Show canvas'
+		canvasWrapper.setAttribute('data-canvas', 'closed')
 	} else {
 		canvasWrapper?.appendChild(canvas)
+		toggleCanvasButton.innerText = 'Hide canvas'
+		canvasWrapper.setAttribute('data-canvas', 'open')
 	}
 })
